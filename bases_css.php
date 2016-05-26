430aa96159a6ecd8421cc482455ea93f24f10f0e
GrCOTE7 <GrCOTE7@GMail.com>
2016-05-26 05:39:58 +0200

    Init

---
+++ bases_css.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fr" class="bases_css">
+<head>
+  <meta charset="UTF-8">
+  <title>CSS | LC</title>
+  <link rel="stylesheet" href="css/bases.css">
+</head>
+
+<body>
+<section>
+
+  <h1>Page <em>stylisee</em></h1>
+
+  <p class="intro">Premier <span>paragraphe</span></p>
+  <p class="intro">Deuxième paragraphe</p>
+
+  <div class="paragraphg7">
+    <img src="imgs/fille.jpg" alt="un belle fille" class="flottant">
+    <p>Texte de l'image
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, alias, doloremque error est explicabo laborum
+      libero minima neque non numquam officia optio perferendis provident quasi repellendus, repudiandae tenetur
+      veritatis vero. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos dolor explicabo,
+      inventore</p>
+    <p class="autreparagraphe">
+      ex facilis hic impedit iste iure laboriosam laudantium minima mollitia nam natus
+      non numquam odit quasi sunt tempore vel, veniam vero voluptatibus. Accusantium beatae delectus eveniet ex
+      fugit
+      inventore itaque iusto minimax neque obcaecati odit placeat porro, quia repudiandae, sint? Accusantium animi
+      dignissimos doloremque minus mollitia! A, alias architecto atque cumque debitis deserunt eveniet facilis
+      harum
+      molestiae necessitatibus officiis optio perspiciatis praesentium provident quae quam quas, quis quos,
+      reiciendis
+      rerum!</p>
+  </div>
+
+</section>
+</body>
+</html>
---
+++ basesindex.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <title>CSS | LC</title>
+  <link rel="stylesheet" href="css/bases.css">
+</head>
+<body>
+<section>
+  <h1>Mon super site<h2>h2 dans un h1</h2></h1>
+  <h2>Intro</h2>
+  <p class="monImg">Bonjour, et bienvenue sur mon site !</p>
+  <p class="paragraphg7">Mon texte développé par <a target="_blank" href="http://lmgtfy.com?q=GrCOTE7">moi</a></p>
+</section>
+</body>
+</html>
